1b84eb4a841e527b3d8df17a830482ea2f1dbffc
Francis Sagbo <francissagbo1@gmail.com>
2026-05-22 02:54:21 +0200

    Ajout de HotDeal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import HeroBanner from "@/components/home/HeroBanner"
 import NewProducts from "@/components/home/NewProducts"
+import HotDeal from "@/components/home/HotDeal"
 
 export default function Home() {
   return (
     <main>
       <HeroBanner />
       <NewProducts />
+      <HotDeal />
     </main>
   )
 }
